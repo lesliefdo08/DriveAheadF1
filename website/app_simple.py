038cc23def565d84cd88a5877c37e3e75d864bb8
Leslie Fernando <lesliefernando68@gmail.com>
2025-09-14 19:16:55 +0530

    Clean up: Remove all deployment configs, keep only Git and original Flask app

--- website/app_simple.py
+++ website/app_simple.py
@@ -1384,5 +1384,4 @@ if __name__ == '__main__':
     print("=" * 60)
     
     # Run the application
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
