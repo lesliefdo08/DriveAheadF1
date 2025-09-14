d86336f5829674b60c4b45124e6b4bfba902e025
Leslie Fernando <lesliefernando68@gmail.com>
2025-09-14 16:32:28 +0530

    Fix Railway deployment: Dynamic PORT configuration and production settings

--- website/app_simple.py
+++ website/app_simple.py
@@ -1384,4 +1384,4 @@ if __name__ == '__main__':
     print("=" * 60)
     
     # Run the application
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
